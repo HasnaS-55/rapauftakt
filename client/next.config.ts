d071ca7691ebc82a1a02f8d8fe09a1c505eeab48
HasnaS-55 <salihihasna02@gmail.com>
2026-05-18 16:25:43 +0100

    --

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "pub-0218d5cd176b4ab3a44627e4fad3872c.r2.dev",
+        pathname: "/**",
       },
     ],
   },
